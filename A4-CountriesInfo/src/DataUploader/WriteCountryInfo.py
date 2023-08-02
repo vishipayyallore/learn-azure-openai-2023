6fc4b8da706a136547e97e0dd97fa994f16511f3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-03 00:17:38 +0530

    code

--- A4-CountriesInfo/src/DataUploader/WriteCountryInfo.py
+++ A4-CountriesInfo/src/DataUploader/WriteCountryInfo.py
@@ -1,40 +1,65 @@
+import json
 import pyodbc
+
+from CountryInfo import CountryInfo
 
 from dotenv import dotenv_values
 config_details = dotenv_values(".env")
 
-def insert_country_info(connection_string, country_name, capital_state, national_bird, country_population):
+
+def insert_country_info(connection_string, country_info):
+
     try:
-        # Connect to the SQL Server database
         conn = pyodbc.connect(connection_string)
+        print("Connection to the SQL Server database is successful.")
+
+        # Create a cursor
         cursor = conn.cursor()
 
         # Prepare the SQL statement to execute the stored procedure
         sql = "{CALL usp_insert_country_info (?, ?, ?, ?, ?)}"
 
         # Prepare the parameters for the stored procedure
-        params = (country_name, capital_state, national_bird, country_population, None)
+        params = (country_info.country_name, country_info.capital_state,
+                  country_info.national_bird, country_info.country_population, country_info.country_id)
 
         # Execute the stored procedure to insert data into the table
         cursor.execute(sql, params)
+
+        # Commit the transaction
         conn.commit()
 
-        # Retrieve the output parameter value (CountryId)
-        country_id = params[4]
-        print(f"Data inserted successfully. CountryId: {country_id}")
-    except pyodbc.Error as ex:
-        print("Error:", ex)
+        print("Country information inserted with CountryId:",
+              country_info.country_id)
+
+    except pyodbc.Error as e:
+        print("Error connecting to the SQL Server database:", e)
     finally:
         if conn:
+            # Close the cursor and connection
+            cursor.close()
             conn.close()
+            print("Connection closed.")
+
 
 # Example usage:
 if __name__ == "__main__":
     connection_string = config_details['SQLSERVERCONNECTIONSTRING']
 
-    country_name = "Sample Country"
-    capital_state = "Sample Capital"
-    national_bird = "Sample Bird"
-    country_population = 1000000
+    # Sample JSON object representing country info
+    json_data = '''
+    {
+        "country_name": "Sample Country",
+        "capital_state": "Sample Capital",
+        "national_bird": "Sample Bird",
+        "country_population": 1000000,
+        "country_id": 0
+    }
+    '''
 
-    insert_country_info(connection_string, country_name, capital_state, national_bird, country_population)
+    # Parse the JSON object and create a CountryInfo object
+    country_info_dict = json.loads(json_data)
+    country_info = CountryInfo(**country_info_dict)
+
+    # Insert the country information into the database
+    insert_country_info(connection_string, country_info)
