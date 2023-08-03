fd756edd499a91736096458f3c1dfdc5b027817d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-04 00:16:56 +0530

    Changes

--- A4-CountriesInfo/src/DataUploaderAPI/WriteCountryInfo.py
+++ A4-CountriesInfo/src/DataUploaderAPI/WriteCountryInfo.py
@@ -1,69 +1,69 @@
-import json
-import pyodbc
+# import json
+# import pyodbc
 
-from CountryInfo import CountryInfo
+# from CountryInfo import CountryInfo
 
-from dotenv import dotenv_values
-config_details = dotenv_values(".env")
+# from dotenv import dotenv_values
+# config_details = dotenv_values(".env")
 
 
-def insert_country_info(connection_string, country_info):
+# def insert_country_info(connection_string, country_info):
 
-    try:
-        conn = pyodbc.connect(connection_string)
-        print("Connection to the SQL Server database is successful.")
+#     try:
+#         conn = pyodbc.connect(connection_string)
+#         print("Connection to the SQL Server database is successful.")
 
-        # Create a cursor
-        cursor = conn.cursor()
+#         # Create a cursor
+#         cursor = conn.cursor()
 
-        # Prepare the SQL statement to execute the stored procedure
-        sql = "{CALL usp_insert_country_info (?, ?, ?, ?, ?)}"
+#         # Prepare the SQL statement to execute the stored procedure
+#         sql = "{CALL usp_insert_country_info (?, ?, ?, ?, ?)}"
 
-        # Prepare the parameters for the stored procedure
-        params = (country_info.country_name, country_info.capital_state,
-                  country_info.national_bird, country_info.country_population, country_info.country_id)
+#         # Prepare the parameters for the stored procedure
+#         params = (country_info.country_name, country_info.capital_state,
+#                   country_info.national_bird, country_info.country_population, country_info.country_id)
 
-        # Execute the stored procedure to insert data into the table
-        cursor.execute(sql, params)
+#         # Execute the stored procedure to insert data into the table
+#         cursor.execute(sql, params)
         
-        # Fetch the output parameter value (CountryId)
-        # country_info.country_id = params[4]
-        country_info.country_id = cursor.fetchval()
+#         # Fetch the output parameter value (CountryId)
+#         # country_info.country_id = params[4]
+#         country_info.country_id = cursor.fetchval()
 
-        # Commit the transaction
-        conn.commit()
+#         # Commit the transaction
+#         conn.commit()
 
-        print("Country information inserted with CountryId:",
-              country_info.country_id)
+#         print("Country information inserted with CountryId:",
+#               country_info.country_id)
 
-    except pyodbc.Error as e:
-        print("Error connecting to the SQL Server database:", e)
-    finally:
-        if conn:
-            # Close the cursor and connection
-            cursor.close()
-            conn.close()
-            print("Connection closed.")
+#     except pyodbc.Error as e:
+#         print("Error connecting to the SQL Server database:", e)
+#     finally:
+#         if conn:
+#             # Close the cursor and connection
+#             cursor.close()
+#             conn.close()
+#             print("Connection closed.")
 
 
-# Example usage:
-if __name__ == "__main__":
-    connection_string = config_details['SQLSERVERCONNECTIONSTRING']
+# # Example usage:
+# if __name__ == "__main__":
+#     connection_string = config_details['SQLSERVERCONNECTIONSTRING']
 
-    # Sample JSON object representing country info
-    json_data = '''
-    {
-        "country_name": "Sample Country",
-        "capital_state": "Sample Capital",
-        "national_bird": "Sample Bird",
-        "country_population": 1000000,
-        "country_id": 0
-    }
-    '''
+#     # Sample JSON object representing country info
+#     json_data = '''
+#     {
+#         "country_name": "Sample Country",
+#         "capital_state": "Sample Capital",
+#         "national_bird": "Sample Bird",
+#         "country_population": 1000000,
+#         "country_id": 0
+#     }
+#     '''
 
-    # Parse the JSON object and create a CountryInfo object
-    country_info_dict = json.loads(json_data)
-    country_info = CountryInfo(**country_info_dict)
+#     # Parse the JSON object and create a CountryInfo object
+#     country_info_dict = json.loads(json_data)
+#     country_info = CountryInfo(**country_info_dict)
 
-    # Insert the country information into the database
-    insert_country_info(connection_string, country_info)
+#     # Insert the country information into the database
+#     insert_country_info(connection_string, country_info)
