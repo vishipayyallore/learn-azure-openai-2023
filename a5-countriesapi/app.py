de55b09b136cb5cd6ead63815031258531c6ed49
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-06 15:06:03 +0530

    Testing

--- a5-countriesapi/app.py
+++ a5-countriesapi/app.py
@@ -33,4 +33,5 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    # app.run(host='0.0.0.0', port=8000, debug=True) # During development
+    app.run()  # In production
