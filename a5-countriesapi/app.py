1754c9b1ef03af0effe67a4b52c62f93f7091550
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-07 14:38:03 +0530

    Update app.py

--- a5-countriesapi/app.py
+++ a5-countriesapi/app.py
@@ -37,11 +37,11 @@ def create_app():
     return app
 
 
-# # # mCreate the app and run it during development
-# if __name__ == "__main__":
-#     app = create_app()
-#     app.run(host='0.0.0.0', port=5000, debug=True)  # During development
+# # mCreate the app and run it during development
+if __name__ == "__main__":
+    app = create_app()
+    app.run(host='0.0.0.0', port=5000, debug=True)  # During development
 
-# For production deployment, comment out the above lines and use the one below
-app = create_app()
-app.run()  # In production
+# # For production deployment, comment out the above lines and use the one below
+# app = create_app()
+# app.run()  # In production
